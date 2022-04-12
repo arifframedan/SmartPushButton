efbcf1b40539b3350430c3fe34a1c9594265d936
Ariff Ramedan <ariff.ramedan@gmail.com>
2022-04-12 13:46:33 +0800

    Create usage.ino

---
+++ examples/usage.ino
@@ -0,0 +1,32 @@
+#include <SmartPushButton.h>
+
+SmartPushButton spb;
+
+void setup()
+{
+  Serial.begin (57600); 
+  spb.begin(0);
+}
+
+void loop()
+{
+  spb.process();
+  
+  if (spb.event() == EVENT_SINGLE_PRESSED){
+    Serial.println("SINGLE PRESS");
+    spb.clearEvent();
+  } 
+  else if (spb.event() == EVENT_SINGLE_PRESSED_HOLD){
+    Serial.println("SINGLE PRESS & HOLD");
+    spb.clearEvent();
+  }
+  else if (spb.event() == EVENT_DOUBLE_PRESSED){
+    Serial.println("DOUBLE PRESS");
+    spb.clearEvent();
+  }
+  else if (spb.event() == EVENT_DOUBLE_PRESSED_HOLD){
+    Serial.println("DOUBLE PRESS & HOLD");
+    spb.clearEvent();
+  }
+  
+}
