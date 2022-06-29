e2499d047dd6c5fbfce8be6100ddef713ead1bc2
Ariff Ramedan <ariff.ramedan@gmail.com>
2022-06-29 10:56:46 +0800

    Update SmartPushButton.h
    Add signal to each event

--- src/SmartPushButton.h
+++ src/SmartPushButton.h
@@ -2,29 +2,23 @@
 #define _SMARTPUSHBUTTON_h
 
 #include <Arduino.h>
+#include <Signals.h>
+
+
 
 #define EVENT_SINGLE_PRESSED      1
 #define EVENT_DOUBLE_PRESSED      2
 #define EVENT_SINGLE_PRESSED_HOLD 3
 #define EVENT_DOUBLE_PRESSED_HOLD 4
 
+#define ACTIVE_LOW	0
+#define ACTIVE_HIGH 1
+
 class SmartPushButton {
-  #define PUSHED    0
-  #define NOTPUSHED 1
   
   public:
-    void begin(int Pin){
-      tPin = Pin;
-      pinMode(tPin, INPUT_PULLUP);
-      pMillis = millis();
-    }
-    void process(){
-      unsigned long cMillis = millis();
-      if (cMillis - pMillis >= 10){
-        pMillis = cMillis;
-        stateMachine();
-      }
-    }
+    void begin(int Pin, int Polarity);
+    void process();
     int event(){
       return Event;
     }
@@ -38,9 +32,15 @@ class SmartPushButton {
       longPressInterval = mSec/10;
     }
    
+	Signals<> signalSinglePress;
+	Signals<> signalSinglePressHold;
+	Signals<> signalDoublePress;
+	Signals<> signalDoublePressHold;
+   
   private:
     void stateMachine();
     unsigned long pMillis; 
+	unsigned char PUSHED, NOTPUSHED;
     int tPin;
     int longPressInterval = 200;
     int shortPressInterval = 50;
